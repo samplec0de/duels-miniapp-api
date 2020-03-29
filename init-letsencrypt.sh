081ba134801b360aa3d5578e95811ef48c0391bc
Andrew Moscalev <admin@asimple.ru>
2020-03-29 21:10:26 +0300

    fix(*): fixed all known startup issues

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: docker-compose is not installed.' >&2
+  exit 1
+fi
+
 domains=api.duels.asimple.ru
 rsa_key_size=4096
 data_path="./data/certbot"
@@ -17,18 +22,18 @@ fi
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
 echo "### Creating dummy certificate for $domains ..."
-cert_path="/etc/letsencrypt/live/$domains"
+path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
-    -keyout '$cert_path/privkey.pem' \
-    -out '$cert_path/fullchain.pem' \
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 echo
 
