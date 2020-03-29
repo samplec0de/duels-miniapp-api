2c9cea9e40aeb5fa170a9540143be28458c8a221
Andrew Moscalev <admin@asimple.ru>
2020-03-29 16:53:44 +0300

    fix(letsencypt): change server domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-domains=(example.com www.example.com)
+domains=(api.duels.asimple.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="admin@asimple.ru" # Adding a valid address is strongly recommended
