eedae5ef7c404d46f6a150b541a923bcf8847007
Andrew Moscalev <admin@asimple.ru>
2020-04-01 01:15:03 +0300

    feature(task): added task create method

--- flask_app/task.py
+++ flask_app/task.py
@@ -1,21 +1,9 @@
-from pymongo.database import Database
+from typing import List
 
-ALLOWED_SUBJECTS = ['math', 'russian', 'informatics', 'english']
+from pymongo.database import Database
 
 
 class TaskException(Exception):
-    pass
-
-
-class TaskSubjectIncorrect(TaskException):
-    pass
-
-
-class TaskIdTypeNotInt(TaskException):
-    pass
-
-
-class SubjectTypeNotStr(TaskException):
     pass
 
 
@@ -24,24 +12,33 @@ class TaskNotFound(TaskException):
 
 
 class Task:
-    __slots__ = ('text', 'answer', 'variants')
+    __slots__ = ('text', 'answer', 'variants', 'id', 'subject')
 
     def __init__(self):
+        self.subject = None
         self.text = None
         self.answer = None
         self.variants = None
+        self.id = None
 
-    def from_db(self, task_id: int, subject: str, client: Database):
-        if subject not in ALLOWED_SUBJECTS:
-            raise TaskSubjectIncorrect(f"Subject {subject} is not allowed")
-        if type(task_id) != int:
-            raise TaskIdTypeNotInt(f"Task id type {type(task_id)} is not allowed.")
-        if type(subject) != str:
-            raise SubjectTypeNotStr(f"Subject type {type(task_id)} is not allowed.")
+    def from_db(self, task_id: str, subject: str, client: Database) -> None:
         collection = client[subject]
         meta = collection.find_one({'task_id': {'$in': task_id}})
         if not meta:
             raise TaskNotFound(f"Task with id {task_id} not found.")
+        self.subject = subject
         self.text = meta.get('text')
         self.answer = meta.get('answer')
         self.variants = meta.get('variants')
+        self.id = task_id
+
+    def create(self, subject: str, text: str, variants: List[str], answer: int, client: Database) -> str:
+        self.subject = subject
+        self.text = text
+        self.answer = answer
+        self.variants = variants
+        collection = client[subject]
+        result = collection.insert_one({'text': text, 'answer': answer, 'variants': variants})
+        self.id = result.inserted_id
+        return self.id
+
