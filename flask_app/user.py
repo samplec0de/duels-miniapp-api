419b1e4ba0d6ee741598fca0b0fd4db32b10c78e
Andrew Moscalev <admin@asimple.ru>
2020-04-01 02:25:24 +0300

    feature(user): implemented user class

--- flask_app/user.py
+++ flask_app/user.py
@@ -2,22 +2,21 @@ from pymongo.database import Database
 
 
 class User:
-    __slots__ = ('id', 'vk_id', 'solved_tasks', 'client', 'collection')
+    __slots__ = ('id', 'vk_id', 'tasks', 'client', 'collection')
 
     def __init__(self, vk_id: int, client: Database):
-        self.id = None
-        self.vk_id = vk_id
-        self.solved_tasks = None
-        self.client = client
-        self.collection = self.client['users']['vk_users']
+        self.id = None  # ObjectId из mongo
+        self.vk_id = vk_id  # user_id из vk api
+        self.tasks = None  # словарь task_id, которые были ПОКАЗАНЫ пользователю (task_id: статус)
+        self.collection = client['data']['users']
         data = self._find()
         if data:
-            self.id = data['_id']
-            self.vk_id = data['vk_id']
-            self.solved_tasks = data['solved_tasks']
+            self.id = data.get('_id')
+            self.vk_id = data.get('vk_id')
+            self.tasks = data.get('tasks')
         else:
-            user = {'vk_id': self.vk_id, 'solved_tasks': []}
+            user = {'vk_id': self.vk_id, 'tasks': dict()}
             self.id = self.collection.insert_one(user).inserted_id
 
-    def _find(self) -> Document:
+    def _find(self) -> dict:
         return self.collection.find_one({'vk_id': {'$eq': self.vk_id}})
