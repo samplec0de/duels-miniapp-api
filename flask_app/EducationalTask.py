6a022031d3cc13286ff8fb251e31693eebad28df
Andrew Moscalev <admin@asimple.ru>
2020-04-01 04:06:30 +0300

    refactor(task): renamed client --> database, because conceptually client argument is Database, not client

--- flask_app/EducationalTask.py
+++ flask_app/EducationalTask.py
@@ -15,18 +15,18 @@ class TaskNotFound(TaskException):
 class EducationalTask:
     __slots__ = ('text', 'answer', 'variants', 'id', 'subject', 'weight')
 
-    def __init__(self, task_id: str = None, subject: str = None, client: Database = None):
+    def __init__(self, task_id: str = None, subject: str = None, database: Database = None):
         self.subject = None  # предмет
         self.text = None  # текст вопроса
         self.answer = None  # индекс правильного ответа (с нуля)
         self.variants = None  # массив варивантов ответа
         self.id = None  # ObjectId в mongo
         self.weight = None  # сколько баллов будет начислено в случае верного решения
-        if task_id and subject and client:
-            self.from_db(task_id=task_id, subject=subject, client=client)
+        if task_id and subject and database:
+            self.from_db(task_id=task_id, subject=subject, database=database)
 
-    def from_db(self, task_id: str, subject: str, client: Database) -> None:
-        collection = client[subject]
+    def from_db(self, task_id: str, subject: str, database: Database) -> None:
+        collection = database[subject]
         meta = collection.find_one({'_id': ObjectId(task_id)})
         if not meta:
             raise TaskNotFound(f"Task with id {task_id} not found.")
@@ -37,13 +37,13 @@ class EducationalTask:
         self.id = meta.get('id')
         self.weight = meta.get('weight')
 
-    def create(self, subject: str, text: str, variants: List[str], answer: int, weight: int, client: Database) -> str:
+    def create(self, subject: str, text: str, variants: List[str], answer: int, weight: int, database: Database) -> str:
         self.subject = subject
         self.text = text
         self.answer = answer
         self.variants = variants
         self.weight = weight
-        collection = client[subject]
+        collection = database[subject]
         task_data = {
             'text': text,
             'answer': answer,
