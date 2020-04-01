fbf245d00aabf5716b24cc2b7dd92b5383573ddf
Andrew Moscalev <admin@asimple.ru>
2020-04-01 03:03:15 +0300

    refactor(user): add type annotations

--- flask_app/user.py
+++ flask_app/user.py
@@ -25,14 +25,14 @@ class User:
     def _find(self) -> dict:
         return self.collection.find_one({'vk_id': {'$eq': self.vk_id}})
 
-    def add_task(self, task_id: str):
+    def add_task(self, task_id: str) -> None:
         self.tasks[ObjectId(task_id)] = (TASK_PENDING, unix())
         self.collection.find_one_and_update({'_id': self.id}, {'tasks': self.tasks})
 
-    def task_success(self, task_id: str):
+    def task_success(self, task_id: str) -> None:
         self.tasks[ObjectId(task_id)] = (TASK_SUCCESS, unix())
         self.collection.find_one_and_update({'_id': self.id}, {'tasks': self.tasks})
 
-    def task_failed(self, task_id: str):
+    def task_failed(self, task_id: str) -> None:
         self.tasks[ObjectId(task_id)] = (TASK_FAILED, unix())
         self.collection.find_one_and_update({'_id': self.id}, {'tasks': self.tasks})
